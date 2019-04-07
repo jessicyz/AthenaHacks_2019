aea757feaeebafe7d60042343a9fb6a999986df3
Ariadne Dimarogonas <adimarogona@gmail.com>
2019-04-07 03:24:18 -0700

    Add files via upload

---
+++ AthenaHacks2019/Assets/Scripts/timeScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Text timeText;
+    [SerializeField] Slider slider;
+    // time in seconds not minutes
+    private float startTime = 6;
+    private bool stahp = true;
+    private float currentTime = 0.0f;
+    private float currentTimeHours = 0.0f;
+    private float currentTimeSeconds = 0.0f;
+
+
+    private void Start()
+    {
+        slider.value = 0;
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (currentTime <= 1)
+        {
+
+            stahp = false;
+            currentTime = 0.0f;
+            slider.value = 1;
+
+        }
+        if (stahp) {
+            slider.value += Time.deltaTime/startTime;
+            //val += (float) Time.deltaTime;
+        currentTime -= Time.deltaTime;
+            currentTimeHours = currentTime / 60;
+        currentTimeSeconds = (currentTime%60)-1;
+           //currentTimeSeconds -= Time.fixedDeltaTime;
+        timeText.text = currentTimeHours.ToString("0") + ":" + currentTimeSeconds.ToString("0");
+    }
+       
+    }
+}
