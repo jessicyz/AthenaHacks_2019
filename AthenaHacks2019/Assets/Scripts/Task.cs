d086e8680af770e185304065ba0d0cee8a96608c
jessicyz <jessicyz@usc.edu>
2019-04-07 02:05:56 -0700

    More UI elements and preliminary scripts

---
+++ AthenaHacks2019/Assets/Scripts/Task.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task
+{
+    //name of task
+    string taskName;
+
+    //what kind of task
+    //0 for premium, 1 for custom
+    int taskType;
+
+    //amount you gain from completing the task
+    int amountGained;
+
+    //number of times you can do a task a day
+    int numTimesPerDay;
+    
+    //number of time you've done the task today
+    int numTimesToday = 0;
+    
+    //last time task was done
+    //date and time class
+
+    //how long it takes to complete a task
+    // num minutes/seconds
+
+    //amount of time cooldown (to prevent people from brushing their teeth and immediately brushing again)
+    //num minutes/seconds
+    public Task(){
+
+    }
+
+    //return true if it's not on cooldown and you haven't done the task more than the max
+    public bool CheckCooldown(){
+        if(numTimesToday > numTimesPerDay){
+            return false;
+        }
+        //if the time between the last time you did a task and the current time is < cooldown time, 
+        //return false
+        
+        return true;
+    }
+
+}
---
+++ AthenaHacks2019/Assets/Scripts/Villager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villager
+{
+    //name of villager
+    string name;
+    //name of sprite. 
+    //gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load("img1", typeof(Sprite)) as Sprite;
+    string sprite;
+    //how rare it is, for determining rates
+    //int rarity;
+    //name of task, in order to find the task that it modifies
+    string taskName;
+    //amount it increases the amount of currence you get by.
+    float bonus;
+    public Villager(){
+
+    }
+    public Villager(string name_, string sprite_, string taskName_, float bonuss_){
+        name = name_;
+        sprite = sprite_;
+        //rarity = rarity_;
+        taskName = taskName_;
+        bonus = bonuss_;
+    }
+}
